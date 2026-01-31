5a9dd8f5b618d8eb8ba00d34f47295a647883d66
Einars Mazurs <einars.mazurs@gmail.com>
2026-01-31 22:19:33 +0200

    config for Render

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const cors = require('cors')
 const db = require('./db')
 
-const PORT = process.env.BACKEND_PORT || 4000
+const PORT = process.env.PORT || process.env.BACKEND_PORT || 4000
 
 const app = express()
 app.use(cors())
